89e99a05a800b882a06ddcc4e75da625e787ae9f
tkemaladzenino <tkemaladzenino71@gmail.com>
2023-05-01 08:41:34 -0800

    update

---
+++ jd-7.js
@@ -0,0 +1,17 @@
+
+
+
+
+var number = [1, 2, -9, -56, 34, 77, -90, -11, -1, -2, -5, -9];
+var index = 0;
+
+
+while (index < number.length) {
+
+    if (number[index] < 0) {
+
+        document.write(number[index] + "<br>");
+    }
+
+    index++;
+}
